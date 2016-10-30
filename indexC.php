d66c81e1228185812e90d001fe39ba85a6ea42f1
Jody Moore <jodymoore@g.harvard.edu>
2016-10-30 13:59:35 -0500

    initial commit

---
+++ indexC.php
@@ -0,0 +1,30 @@
+<?php
+    // configuration
+$region='%region%';
+$bucket='%bucket%';
+$tmp='';
+$categories=NULL;
+$table_name = 'AWS-Services';
+    // configuration
+require 'aws-autoloader.php';
+require("/includes/config.php");
+date_default_timezone_set('UTC');
+try {
+   $sdk = new Aws\Sdk([
+         'region' => $region,
+      'version'   => 'latest'
+   ]);
+
+   $dynamodb = $sdk->createDynamoDb();
+   $response = $dynamodb->describeTable(array('TableName' => $table_name));
+   $categories = $dynamodb->scan(array(   'TableName' => $table_name, 
+                  'AttributeValueList' => array(array('S' => 'Category'))
+   ));
+}
+catch(Exception $e) {
+   $tmp=$e->getMessage();
+   $categories=NULL;
+}
+ 
+   render("/templates/select_form.php");
+?> 
