2a67ddc671421a6079b5fd5adde11845487d3580
Jody Moore <jodymoore@g.harvard.edu>
2016-11-02 13:24:56 -0500

    commit 2

--- logout.php
+++ logout.php
@@ -3,12 +3,23 @@
     // configuration
     require("../includes/config.php"); 
 
+    date_default_timezone_set('America/NewYork');
+    $region='us-east-1';
+    $bucket='%bucket%';
+    $tmp='';
 
+    	$sdk = new Aws\Sdk([
+	    'region'   => 'us-east-1',
+	    'version'  => 'latest'
+	]);
+
+    $dynamodb = $sdk->createDynamoDB();
 
     $idToken  = $_SESSION["id"];
             
     $table_name2 = 'rover_log_out';
-    $dateTime =  (string)date('m/d/Y G:m:s'); 
+    
+    $dateTime =  (string)date.timezone_location_get(); 
     // update DynamoDB table with id and time()     
      $response = $dynamodb->putItem([
         'TableName' => $table_name2,
