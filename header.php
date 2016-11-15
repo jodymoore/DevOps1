7a95bff60ceec07b04701172ddaa41157ca401d9
Jody Moore <jodymoore@g.harvard.edu>
2016-11-15 15:37:32 -0600

    initial commit

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <link href="s3.amazonaws.com/ncrov/public/css/bootstrap.css" rel="stylesheet"/>
+        <link href="s3.amazonaws.com/ncrov/public/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="s3.amazonaws.com/ncrov/public/css/bootstrap-theme.min.css" rel="stylesheet"/>
+        <link href="s3.amazonaws.com/ncrov/public/css/styles.css" rel="stylesheet"/>
+        <link href="s3.amazonaws.com/ncrov/public/css/bootstrap.theme.css" rel="stylesheet"/>
+
+        <?php if (isset($title)): ?>
+            <title>Rover1: <?= htmlspecialchars($title) ?></title>
+        <?php else: ?>
+            <title>Rover1</title>
+        <?php endif ?>
+
+        <script src="s3.amazonaws.com/ncrov/public/js/jquery-1.10.2.min.js"></script>
+        <script src="s3.amazonaws.com/ncrov/public/js/bootstrap.min.js"></script>
+        <script src="s3.amazonaws.com/ncrov/public/js/scripts.js"></script>
+        <script src="s3.amazonaws.com/ncrov/public/js/bootstrap.js"></script>
+
+    </head>
+     <body>
+
+        <div class="container">
+
+            <div id="top">
+                <a><img alt="Rover1" src="s3.amazonaws.com/ncrov/public/img/Rover-1.gif"/></a>
+            </div>
