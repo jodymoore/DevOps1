c53bb3e4f63a5ccd2c93f6b1b27bdfd45c405d21
Jody Moore <jodymoore@g.harvard.edu>
2017-01-24 10:16:53 -0600

    change to header

--- header.php
+++ header.php
@@ -3,11 +3,11 @@
 <html>
 
     <head>
-        <link href="public/css/bootstrap.css" rel="stylesheet"/>
-        <link href="public/css/bootstrap.min.css" rel="stylesheet"/>
-        <link href="public/css/bootstrap-theme.min.css" rel="stylesheet"/>
-        <link href="public/css/styles.css" rel="stylesheet"/>
-        <link href="public/css/bootstrap.theme.css" rel="stylesheet"/>
+        <link href="css/bootstrap.css" rel="stylesheet"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet"/>
+        <link href="css/styles.css" rel="stylesheet"/>
+        <link href="css/bootstrap.theme.css" rel="stylesheet"/>
 
         <?php if (isset($title)): ?>
             <title>Rover1: <?= htmlspecialchars($title) ?></title>
@@ -15,10 +15,10 @@
             <title>Rover1</title>
         <?php endif ?>
 
-        <script src="ncrov/public/js/jquery-1.10.2.min.js"></script>
-        <script src="ncrov/public/js/bootstrap.min.js"></script>
-        <script src="ncrov/public/js/scripts.js"></script>
-        <script src="ncrov/public/js/bootstrap.js"></script>
+        <script src="js/jquery-1.10.2.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/scripts.js"></script>
+        <script src="js/bootstrap.js"></script>
 
         
         <!-- http://1000hz.github.io/bootstrap-validator/ -->
@@ -30,5 +30,5 @@
         <div class="container">
 
             <div id="top">
-                <a><img alt="Rover1" src="ncrov/public/img/Rover-1.gif"/></a>
+                <a><img alt="Rover1" src="img/Rover-1.gif"/></a>
             </div>
