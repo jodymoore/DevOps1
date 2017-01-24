64cbb1f3e961ecde1d17dc233fc692196789f1d2
Jody Moore <jodymoore@g.harvard.edu>
2017-01-24 10:10:17 -0600

    change to header

--- header.php
+++ header.php
@@ -3,11 +3,11 @@
 <html>
 
     <head>
-        <link href="s3.amazonaws.com/ncrov/public/css/bootstrap.css" rel="stylesheet"/>
-        <link href="s3.amazonaws.com/ncrov/public/css/bootstrap.min.css" rel="stylesheet"/>
-        <link href="s3.amazonaws.com/ncrov/public/css/bootstrap-theme.min.css" rel="stylesheet"/>
-        <link href="s3.amazonaws.com/ncrov/public/css/styles.css" rel="stylesheet"/>
-        <link href="s3.amazonaws.com/ncrov/public/css/bootstrap.theme.css" rel="stylesheet"/>
+        <link href="public/css/bootstrap.css" rel="stylesheet"/>
+        <link href="public/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="public/css/bootstrap-theme.min.css" rel="stylesheet"/>
+        <link href="public/css/styles.css" rel="stylesheet"/>
+        <link href="public/css/bootstrap.theme.css" rel="stylesheet"/>
 
         <?php if (isset($title)): ?>
             <title>Rover1: <?= htmlspecialchars($title) ?></title>
@@ -15,10 +15,14 @@
             <title>Rover1</title>
         <?php endif ?>
 
-        <script src="s3.amazonaws.com/ncrov/public/js/jquery-1.10.2.min.js"></script>
-        <script src="s3.amazonaws.com/ncrov/public/js/bootstrap.min.js"></script>
-        <script src="s3.amazonaws.com/ncrov/public/js/scripts.js"></script>
-        <script src="s3.amazonaws.com/ncrov/public/js/bootstrap.js"></script>
+        <script src="ncrov/public/js/jquery-1.10.2.min.js"></script>
+        <script src="ncrov/public/js/bootstrap.min.js"></script>
+        <script src="ncrov/public/js/scripts.js"></script>
+        <script src="ncrov/public/js/bootstrap.js"></script>
+
+        
+        <!-- http://1000hz.github.io/bootstrap-validator/ -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.5/validator.min.js"></script>
 
     </head>
      <body>
@@ -26,5 +30,5 @@
         <div class="container">
 
             <div id="top">
-                <a><img alt="Rover1" src="s3.amazonaws.com/ncrov/public/img/Rover-1.gif"/></a>
+                <a><img alt="Rover1" src="ncrov/public/img/Rover-1.gif"/></a>
             </div>
